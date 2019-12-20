08ad465f305fe065bb07e34cca1d452bbd3a781e
hurricane618 <wow618384@hotmail.com>
2019-12-20 23:52:25 +0800

    update website and README

--- Website/README.md
+++ Website/README.md
@@ -2,7 +2,6 @@
 ## 网站环境
 1. Python 3.6
 2. Django 3.0
-3. Mysql 5.7
 ## 安装相关依赖库
 
 `pip install -r requirements.txt`
@@ -10,3 +9,26 @@
 ## 开发
 
 hurricane618
+
+## 一些环境问题
+
+### mysqlclient安装失败
+
+检查自己是否安装mysql或者mysql-client，这个库需要mysql相关程序支持
+
+Mac：`brew install mysql` 或者 `brew install mysql-client`
+
+添加mysql的bin目录进PATH中
+
+`export PATH="/usr/local/mysql/bin:${PATH}"`
+
+再次安装即可解决问题
+
+### 加载动态库问题
+
+在Mac下加载mysql的动态库出现错误，是因为路径找不到的原因，我们给`/usr/local/lib`链接一个动态库即可
+
+`sudo ln -s /usr/local/mysql/lib/libmysqlclient.21.dylib /usr/local/lib/libmysqlclient.21.dylib`
+
+这里的动态库换成自己缺失的
+
--- Website/analysis/views.py
+++ Website/analysis/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        return render(request, 'index.html')
+
+    elif request.method == 'POST':
+
+        data = request.POST['data']
+        r = ''
+        s = ''
+        r_set = []
+        # if request.POST['type'] == 'svm':
+        #     print('svm')
+            # do something
+        r = 'xxxxxx'
+        s = 'success'
+        r_set.append({'result': r, 'status': s, 'model': 'svm'})
+
+        # if request.POST['type'] == 'dl':
+        #     print('dl')
+        r = 'xxxxxx'
+        s = 'success'
+        r_set.append({'result': r, 'status': s, 'model': 'dl'})
+
+        # if request.POST['type'] == 'nb':
+        #     print('nb')
+        r = 'xxxxxx'
+        s = 'success'
+        r_set.append({'result': r, 'status': s, 'model': 'nb'})
+
+        # if request.POST['type'] == 'lstm':
+        #     print('lstm')
+        r = 'xxxxxx'
+        s = 'success'
+        r_set.append({'result': r, 'status': s, 'model': 'lstm'})
+
+
+        # do something
+
+        #return render(request, 'index.html', {'result': r, 'status': s, 'data': data})
+        return render(request, 'index.html', {'r_set': r_set, 'data': data})
+
+    else:
+        return HttpResponse('your method is not valid!')
+
+
